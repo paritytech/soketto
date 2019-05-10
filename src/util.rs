b2326a7d4b6f78ecf0e157023d68d1a5070e609e
Toralf Wittner <tw@dtex.org>
2019-05-10 15:58:33 +0200

    Move utils.rs up one directory.

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,5 @@
 //! General Utilities
+use std::borrow::Cow;
 use std::io;
 #[cfg(test)]
 use std::io::Write;
@@ -52,4 +53,28 @@ pub fn hex_header() -> String {
             "d",
             "e",
             "f")
+}
+
+pub struct Invalid<'a>(pub(crate) Cow<'a, str>);
+
+pub(crate) fn expect_header<'a, T>(r: &http::Request<T>, n: &http::header::HeaderName, v: &str) -> Result<(), Invalid<'a>> {
+    with_header(r, n, move |value| {
+        if unicase::Ascii::new(value) != v {
+            Err(Invalid(Cow::Owned(format!("unexpected header value: {}", n))))
+        } else {
+            Ok(())
+        }
+    })
+}
+
+pub(crate) fn with_header<'a, T, F, R>(r: &http::Request<T>, n: &http::header::HeaderName, f: F) -> Result<R, Invalid<'a>>
+where
+    F: Fn(&str) -> Result<R, Invalid<'a>>
+{
+    r.headers().get(n)
+        .ok_or(Invalid(Cow::Owned(format!("missing header name: {}", n))))
+        .and_then(|value| {
+            value.to_str().map_err(|_| Invalid(Cow::Owned(format!("invalid header: {}", n))))
+        })
+        .and_then(f)
 }
